ce677171ecbe15f826ebe1c61153cd3585f1ad76
techeo <tegaecheo@gmail.com>
2024-01-10 05:42:29 -0800

    calculator task changes

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from calculator_1 import add, subtract, multiply, divide
-
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-
-    addition_result = add(a, b)
-    subtraction_result = subtract(a, b)
-    multiplication_result = multiply(a, b)
-    division_result = divide(a, b)
-
-    print("Result of addition:", addition_result)
-    print("Result of subtraction:", subtraction_result)
-    print("Result of multiplication:", multiplication_result)
-    print("Result of division:", division_result)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
