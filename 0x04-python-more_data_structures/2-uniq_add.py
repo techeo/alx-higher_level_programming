c44e0ff881bccebab56c41c6e183eb5232644588
techeo <tegaecheo@gmail.com>
2024-01-12 02:13:36 -0800

    2-uniq_add.py task changes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    unique_set = set()
-    
-    for num in my_list:
-        unique_set.add(num)
-    
-    return sum(unique_set)
+    uniq_list = set(my_list)
+    number = 0
+
+    for i in uniq_list:
+        number += i
+
+    return (number)
