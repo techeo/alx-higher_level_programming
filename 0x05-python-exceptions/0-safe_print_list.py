cfb7c3d45ec3f012bf751f045db639d69ad9c39c
techeo <tegaecheo@gmail.com>
2024-01-24 10:33:10 -0800

    task1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            printed += 1
+            printed_elements += 1
         except:
             continue
     print()
