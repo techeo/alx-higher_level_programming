c938e101bbfb6b5843df568afd1f92e68873f83d
techeo <tegaecheo@gmail.com>
2024-02-06 09:21:04 -0800

    10-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student:
+    """
+    Class that defines properties of student.
+
+    Attributes:
+        first_name (str): first name of student.
+        last_name (int): last name of student.
+        age (int): age of student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (int): last name of student.
+            age (int): age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of the Student.
+        """
+        return self.__dict__
