7f206b27ea940cde55bcff4c5564ace547402003
techeo <tegaecheo@gmail.com>
2024-04-29 06:09:42 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read().decode('utf-8')
+
+print("- Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+
