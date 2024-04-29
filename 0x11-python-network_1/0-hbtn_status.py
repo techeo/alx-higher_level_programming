3c7d5dcd345f1342675c3a638a49f0103049c2c2
techeo <tegaecheo@gmail.com>
2024-04-29 07:42:12 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,9 @@
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response:
-    body_bytes = response.read()
-    body_utf8 = body_bytes.decode('utf-8')
-
-print("Body response:")
-print("\t- type:", type(body_bytes))
-print("\t- content:", body_bytes)
-print("\t- utf8 content:", body_utf8)
-
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
